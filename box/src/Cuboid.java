6c3a3c281c779967717f489fb809f9af3dc07224
Balazs Stegmar <stegmarb@gmail.com>
2017-04-10 09:48:25 +0200

    feat(Cuboid): Cuboid app now has all methods and a constructor

---
+++ box/src/Cuboid.java
@@ -0,0 +1,19 @@
+public class Cuboid {
+  int aSide;
+  int bSide;
+  int cSide;
+
+  public int getSurface() {
+    return ((aSide*bSide)+(bSide*cSide)+(cSide+aSide)*2);
+  }
+
+  public int getVolume() {
+    return aSide*bSide*cSide;
+  }
+
+  public Cuboid(int aSide, int bSide, int cSide) {
+    this.aSide = aSide;
+    this.bSide = bSide;
+    this.cSide = cSide
+  }
+}
