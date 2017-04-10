5836ed25e53761066161ca7b919f5492365331fe
Balazs Stegmar <stegmarb@gmail.com>
2017-04-10 10:42:04 +0200

    init(PirateCounter): program now can select the pirates according the given conditions

--- pirates/src/PirateCounter.java
+++ pirates/src/PirateCounter.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class PirateCounter {
 
-  class Pirate {
+  static class Pirate {
     String name;
     boolean hasWoodenLeg;
     int gold;
@@ -17,17 +18,22 @@ public class PirateCounter {
   public static void main(String... args){
     ArrayList<Pirate> pirates = new ArrayList<>();
 
-    // Given this list...
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
 
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+    System.out.println(getGivenPirates(pirates));
+  }
 
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
+  public static List<String> getGivenPirates(List<Pirate> pirates) {
+    List<String> names = new ArrayList<>();
+    for (Pirate pirate : pirates) {
+      if (pirate.hasWoodenLeg && pirate.gold > 15) {
+        names.add(pirate.name);
+      }
+    }
+    return names;
   }
 }
