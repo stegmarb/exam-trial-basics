c1566a3ea9be042f827c055300b8a2c75cecc688
Balazs Stegmar <stegmarb@gmail.com>
2017-04-10 10:50:51 +0200

    init(PirateCounter): Pirate class has been outsourced into a new file

---
+++ pirates/src/Pirate.java
@@ -0,0 +1,11 @@
+class Pirate {
+  String name;
+  boolean hasWoodenLeg;
+  int gold;
+
+  Pirate(String name, boolean hasWoodenLeg, int gold) {
+    this.name = name;
+    this.hasWoodenLeg = hasWoodenLeg;
+    this.gold = gold;
+  }
+}
--- pirates/src/PirateCounter.java
+++ pirates/src/PirateCounter.java
@@ -2,28 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PirateCounter {
-
-  static class Pirate {
-    String name;
-    boolean hasWoodenLeg;
-    int gold;
-
-    Pirate(String name, boolean hasWoodenLeg, int gold) {
-      this.name = name;
-      this.hasWoodenLeg = hasWoodenLeg;
-      this.gold = gold;
-    }
-  }
-
   public static void main(String... args){
     ArrayList<Pirate> pirates = new ArrayList<>();
-
     pirates.add(new Pirate("Olaf", false, 12));
     pirates.add(new Pirate("Uwe", true, 9));
     pirates.add(new Pirate("Jack", true, 16));
     pirates.add(new Pirate("Morgan", false, 17));
     pirates.add(new Pirate("Hook", true, 20));
-
     System.out.println(getGivenPirates(pirates));
   }
 
